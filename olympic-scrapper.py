0129f47425721b7cc1a38bdc019b7c5e629d27a7
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-02-02 16:03:38 +0100

    to_csv method + indentation

--- olympic-scrapper.py
+++ olympic-scrapper.py
@@ -1,71 +1,94 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
+from numpy import empty
+import pandas as pd
+
 
 class olympicScrapper:
     url_part1 = 'https://olympics.com/en/olympic-games/'
     url_part2 = '/results'
-    def __init__(self,city_host='',year=0) -> None:
-        if(city_host=='' and year==0):
+
+    def __init__(self, city_host='', year=0) -> None:
+        if(city_host == '' and year == 0):
             raise OlympicException('City_host and a year needed')
-        if((city_host=='' or year==0)):
+        if((city_host == '' or year == 0)):
             raise OlympicException('city_host-year pair needed')
-        if not isinstance(city_host,str):
+        if not isinstance(city_host, str):
             raise OlympicException('city_host must be a string')
-        if not isinstance(year,int):
+        if not isinstance(year, int):
             raise OlympicException('Year must be an int')
-        self.city_host=city_host.lower()
-        self.year=year
+        self.city_host = city_host.lower()
+        self.year = year
+
     def __createUrl(self):
-        url=self.url_part1+self.city_host+'-'+self.year+self.url_part2
+        url = self.url_part1+self.city_host+'-'+self.year+self.url_part2
         return url
+
     def __getPageSoup(self):
-        olympic_url=self.__createUrl()
+        olympic_url = self.__createUrl()
         olympic_data = urlopen(olympic_url)
         olympic_html = olympic_data.read()
         olympic_data.close()
-        page_soup=soup(olympic_html,'html.parser')
+        page_soup = soup(olympic_html, 'html.parser')
         return page_soup
+    # repeated code , find a way to simplify it
+
     def __getResult(self):
-        result=[]
-        for node in self.__getPageSoup().findAll('div',{'data-cy':'medal'}):
+        result = []
+        for node in self.__getPageSoup().findAll('div', {'data-cy': 'medal'}):
             result.append(''.join(node.findAll(text=True)))
-        for i in range(0,len(result)):
-            if result[i]=='-':
-                result[i]='0'
-        return result    
+        for i in range(0, len(result)):
+            if result[i] == '-':
+                result[i] = '0'
+        return result
+
     def __getCountries(self):
-        countries=[]
-        for node in self.__getPageSoup().findAll('span',{'data-cy':'country-name'}):
+        countries = []
+        for node in self.__getPageSoup().findAll('span', {'data-cy': 'country-name'}):
             countries.append(''.join(node.findAll(text=True)))
         return countries
+
     def __getGoldMedals(self):
-        gold_medals=[]
-        result=self.__getResult()
-        for i in range(0,len(result),4):
+        gold_medals = []
+        result = self.__getResult()
+        for i in range(0, len(result), 4):
             gold_medals.append(result[i])
         return gold_medals
+
     def __getSilverMedals(self):
-        silver_medals=[]
-        result=self.__getResult()
-        for i in range(1,len(result),4):
+        silver_medals = []
+        result = self.__getResult()
+        for i in range(1, len(result), 4):
             silver_medals.append(result[i])
         return silver_medals
+
     def __getBronzeMedals(self):
-        bronze_medals=[]
-        result=self.__getResult()
-        for i in range(2,len(result),4):
+        bronze_medals = []
+        result = self.__getResult()
+        for i in range(2, len(result), 4):
             bronze_medals.append(result[i])
         return bronze_medals
+
     def __getTotalMedals(self):
-        total_medals=[]
-        result=self.__getResult()
-        for i in range(3,len(result),4):
+        total_medals = []
+        result = self.__getResult()
+        for i in range(3, len(result), 4):
             total_medals.append(result[i])
         return total_medals
+
+    def to_csv(self, filename):
+        data = {
+            'Country': self.__getCountries(),
+            'gold_medals': self.__getGoldMedals(),
+            'silver_medals': self.__getSilverMedals(),
+            'bronze_medals': self.__getBronzeMedals(),
+            'total_medals': self.__getTotalMedals()
+        }
+        df = pd.DataFrame(data)
+        df.to_csv(filename, index=False)
 class OlympicException(Exception):
-    def __init__(self,message) -> None:
-        self.message=message
+    def __init__(self, message) -> None:
+        self.message = message
 
 
-
-ol=olympicScrapper(year=2020)
+ol = olympicScrapper(year=2020)
