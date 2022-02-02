a418390395f9a1c95e641ef8fa2e87a98b66ebfd
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-02-02 13:29:39 +0100

    init function

--- olympic-scrapper.py
+++ olympic-scrapper.py
@@ -1,17 +1,19 @@
 class olympicScrapper:
     url_part1 = 'https://olympics.com/en/olympic-games/'
     url_part2 = '/results'
-    def __init__(self,*args,**kwargs) -> None:
-        if args.count() == 0:
-            raise OlympicException('No parameters given')            
-        self.url=kwargs.get('url')
-        self.city_host=str(kwargs.get('city_host')).lower()
-        self.year=int(kwargs.get('year'))
-
-
+    def __init__(self,url='',city_host='',year=0) -> None:
+        if(city_host=='' and year==0):
+            raise OlympicException('City_host and a year needed')
+        if((city_host=='' or year==0)):
+            raise OlympicException('city_host-year pair needed')
+        self.url=url
+        self.city_host=city_host
+        self.year=year
+        
 class OlympicException(Exception):
     def __init__(self,message) -> None:
         self.message=message
 
 
 
+ol=olympicScrapper(year=2020)
