355da2b92bbbc99bb72d5be6c7d65f9e6252320b
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-02-02 12:59:11 +0100

    Exception for init

--- olympic-scrapper.py
+++ olympic-scrapper.py
@@ -1,7 +1,17 @@
 class olympicScrapper:
+    url_part1 = 'https://olympics.com/en/olympic-games/'
+    url_part2 = '/results'
     def __init__(self,*args,**kwargs) -> None:
+        if args.count() == 0:
+            raise OlympicException('No parameters given')            
         self.url=kwargs.get('url')
         self.city_host=str(kwargs.get('city_host')).lower()
         self.year=int(kwargs.get('year'))
+
+
+class OlympicException(Exception):
+    def __init__(self,message) -> None:
+        self.message=message
+
 
 
