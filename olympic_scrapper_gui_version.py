52fb4edf5605a91b940af429aa57ab40655b087c
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-05-03 14:20:19 +0200

    Information label + renaming of variable

--- olympic_scrapper_gui_version.py
+++ olympic_scrapper_gui_version.py
@@ -17,39 +17,49 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        city_label=tk.Entry(root)
-        city_label["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        city_label["font"] = ft
-        city_label["fg"] = "#333333"
-        city_label["justify"] = "center"
-        city_label["text"] = "Enter City name"
-        city_label.place(x=40,y=170,width=164,height=30)
-
-        year_label=tk.Entry(root)
-        year_label["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        year_label["font"] = ft
-        year_label["fg"] = "#333333"
-        year_label["justify"] = "center"
-        year_label["text"] = "Enter Year"
-        year_label.place(x=260,y=170,width=178,height=30)
-
-        city_entry=tk.Label(root)
+        city_entry=tk.Entry(root)
+        city_entry["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
         city_entry["font"] = ft
         city_entry["fg"] = "#333333"
         city_entry["justify"] = "center"
-        city_entry["text"] = "City"
-        city_entry.place(x=80,y=140,width=70,height=25)
+        city_entry["text"] = "Enter City name"
+        city_entry.place(x=40,y=170,width=164,height=30)
 
-        year_entry=tk.Label(root)
+        year_entry=tk.Entry(root)
+        year_entry["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
         year_entry["font"] = ft
         year_entry["fg"] = "#333333"
         year_entry["justify"] = "center"
-        year_entry["text"] = "Year"
-        year_entry.place(x=310,y=140,width=70,height=25)
+        year_entry["text"] = "Enter Year"
+        year_entry.place(x=260,y=170,width=178,height=30)
+
+        city_label=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        city_label["font"] = ft
+        city_label["fg"] = "#333333"
+        city_label["justify"] = "center"
+        city_label["text"] = "City"
+        city_label.place(x=80,y=140,width=70,height=25)
+
+        #information label
+        info_label=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        info_label["font"] = ft
+        info_label["fg"] = "#333333"
+        info_label["justify"] = "center"
+        info_label["text"] = "SEX"
+        info_label.place(x=210,y=350,width=70,height=25)
+
+
+        year_label=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        year_label["font"] = ft
+        year_label["fg"] = "#333333"
+        year_label["justify"] = "center"
+        year_label["text"] = "Year"
+        year_label.place(x=310,y=140,width=70,height=25)
 
         send_button=tk.Button(root)
         send_button["bg"] = "#f0f0f0"
