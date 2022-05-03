c9799ec0447a558e14af1dcefb0289cbdff35858
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-05-03 14:14:22 +0200

    Olympic scrapper class and exception

--- olympic_scrapper_gui_version.py
+++ olympic_scrapper_gui_version.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+import pandas as pd
 import tkinter as tk
 import tkinter.font as tkFont
 
@@ -60,7 +63,93 @@ class App:
 
     def send_button_command(self):
         print("command")
+class OlympicException(Exception):
+    def __init__(self, message) -> None:
+        self.message = message
+class OlympicScrapper:
+    url_part1 = 'https://olympics.com/en/olympic-games/'
+    url_part2 = '/medals'
+    def __init__(self,city_host,year) -> None:
+        self.city_host=city_host
+        self.year=year
+        self.page_soup=self.getPageSoup()
+        self.result=self.result
+        pass
+    def createUrl(self):
+        url = self.url_part1+self.city_host+'-'+str(self.year)+self.url_part2
+        return url
+    
+    # get the soup of a page
+    def getPageSoup(self):
+        olympic_url = self.createUrl()
+        olympic_data = urlopen(olympic_url)
+        real_url = str(olympic_data.geturl())
+        if real_url != olympic_url:
+            # the result of the olympic is empty
+            # the first editions were not so official
+            empty_result_url = self.url_part1 + \
+                self.city_host+'-'+str(self.year)+'/results'
+            if real_url == empty_result_url:
+                return None
+            else:
+                err = "Olympic does not exist:" + self.city_host+"-"+self.year
+                raise OlympicException(err)
+        olympic_html = olympic_data.read()
+        olympic_data.close()
+        page_soup = soup(olympic_html, 'html.parser')
+        return page_soup
+    def getResult(self):
+        result = []
+        for node in self.page_soup.findAll('div', {'data-cy': 'medal'}):
+            result.append(''.join(node.findAll(text=True)))
+        for i in range(0, len(result)):
+            if result[i] == '-':
+                result[i] = '0'
+        return result
+    def getCountries(self):
+        countries = []
+        for node in self.page_soup.findAll('span', {'data-cy': 'country-name'}):
+            countries.append(''.join(node.findAll(text=True)))
+        return countries
+    # get medals data based on the type
+    def getMedals(self,medal_type:str):
+        if medal_type=="Gold":
+            gold_medals = []
+            for i in range(0, len(self.result), 4):
+                gold_medals.append(self.result[i])
+            return gold_medals
+            
+        elif medal_type=="Silver":
+            silver_medals = []
+            for i in range(1, len(self.result), 4):
+                silver_medals.append(self.result[i])
+            return silver_medals
+            
+        elif medal_type=="Bronze":
+            bronze_medals = []
+            for i in range(2, len(self.result), 4):
+                bronze_medals.append(self.result[i])
+            return bronze_medals
 
+        elif medal_type=="Total":
+            total_medals = []
+            for i in range(3, len(self.result), 4):
+                total_medals.append(self.result[i])
+            return total_medals
+        else:
+            raise OlympicException("Medal type not recognized")
+        
+    def olympic_data(self) -> pd.DataFrame:
+
+        data = {
+            'countries': self.getCountries(),
+            'gold_medals': self.getMedals(medal_type='gold'),
+            'silver_medals': self.getMedals(medal_type='silver'),
+            'bronze_medals': self.getMedals(medal_type='bronze'),
+            'total_medals': self.getMedals(medal_type='total'),
+        }
+        df = pd.DataFrame(data)
+        return df
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
